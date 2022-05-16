926da6c016dd2d6a71dbabf11522b3237a3cc402
Yura Alhovka <ualhovka@gmail.com>
2022-05-16 23:35:48 +0300

    Babel corejs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+    
     import tabs from './modules/tabs';
     import forms from './modules/forms';
     import calc from './modules/calc';
